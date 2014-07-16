c0e028f388e21fdd807394081ddbfec5ae3f5347
13610a <beik.kyeong@gmail.com>
2014-07-16 14:51:33 +0900

    This is my homepage~~^*^

---
+++ js/script.js
@@ -0,0 +1,75 @@
+$(document).ready(function(){
+
+	$('#a11').mouseenter(function(){
+		 $(this).animate({ top:'-=50px'},1000);
+		});
+	$('#a11').mouseleave(function(){
+		$(this).animate({ top:'+=50px'},1000);
+	});
+
+
+	$('#a22').mouseenter(function(){
+
+		 $(this).animate({ top:'-=50px'},1000);
+		});
+	$('#a22').mouseleave(function(){
+		$(this).animate({ top:'+=50px'},1000);
+	});
+
+
+
+	$('#a33').mouseenter(function(){
+
+		 $(this).animate({ top:'-=50px'},1000);
+		});
+	$('#a33').mouseleave(function(){
+		$(this).animate({ top:'+=50px'},1000);
+	});
+
+
+	$('#a44').mouseenter(function(){
+
+		 $(this).animate({ top:'-=50px'},1000);
+		});
+	$('#a44').mouseleave(function(){
+		$(this).animate({ top:'+=50px'},1000);
+	});
+
+
+	$('#a55').mouseenter(function(){
+
+		 $(this).animate({ top:'+=50px'},1000);
+		});
+	$('#a55').mouseleave(function(){
+		$(this).animate({ top:'-=50px'},1000);
+	});
+
+
+	$('#a66').mouseenter(function(){
+
+		 $(this).animate({ top:'+=50px'},1000);
+		});
+	$('#a66').mouseleave(function(){
+		$(this).animate({ top:'-=50px'},1000);
+	});
+
+
+	$('#a77').mouseenter(function(){
+
+		 $(this).animate({ top:'+=50px'},1000);
+		});
+	$('#a77').mouseleave(function(){
+		$(this).animate({ top:'-=50px'},1000);
+	});
+
+
+	$('#a88').mouseenter(function(){
+
+		 $(this).animate({ top:'+=50px'},1000);
+		});
+	$('#a88').mouseleave(function(){
+		$(this).animate({ top:'-=50px'},1000);
+	});
+
+
+});
